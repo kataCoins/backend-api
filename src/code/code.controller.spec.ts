4157a602eaddaf0f4a539f6600d603dcc18c82eb
Nathanael TO <nathanaelto.pro@gmail.com>
2023-01-08 17:39:36 +0100

    Add ci

--- src/code/code.controller.spec.ts
+++ src/code/code.controller.spec.ts
@@ -1,14 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CodeController } from './code.controller';
 import { CodeService } from './services/code.service';
+import { FileStorageService } from './services/file-storage.service';
+import { FileUtilsService } from './services/file-utils.service';
+import { ExecPythonService } from './services/exec-python.service';
+import { ExecJavaScriptService } from './services/exec-java-script.service';
+import { HttpModule } from '@nestjs/axios';
 
 describe('CodeController', () => {
   let controller: CodeController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        HttpModule.register({
+          timeout: 10000,
+          maxRedirects: 5,
+        }),
+      ],
       controllers: [CodeController],
-      providers: [CodeService],
+      providers: [
+        CodeService,
+        FileStorageService,
+        FileUtilsService,
+        ExecPythonService,
+        ExecJavaScriptService,
+      ],
     }).compile();
 
     controller = module.get<CodeController>(CodeController);
