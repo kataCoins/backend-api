b6d51f475b2180a9b1c1f6ae727508df8cafeede
Nathanael TO <nathanaelto.pro@gmail.com>
2023-01-14 18:59:53 +0100

    Fix CORS + add http client prod test

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
